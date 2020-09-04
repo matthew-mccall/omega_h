13cbf6c765b2c385380a0b80dad63efc8e91fc43
Cameron Smith <smithc11@rpi.edu>
2020-09-04 10:03:30 -0700

    Revert "don't use __constant__ with hipcc"
    This reverts commit a4b907a42af78d0dd06fc5d28bc82c0771239e03.

--- src/Omega_h_macros.h
+++ src/Omega_h_macros.h
@@ -44,11 +44,7 @@
 #define OMEGA_H_INLINE_BIG OMEGA_H_INLINE
 #define OMEGA_H_DEVICE __device__ inline
 #define OMEGA_H_LAMBDA [=] __device__
-#if defined(OMEGA_H_USE_CUDA)
-  #define OMEGA_H_CONSTANT_DATA __constant__
-#else
-  #define OMEGA_H_CONSTANT_DATA
-#endif
+#define OMEGA_H_CONSTANT_DATA __constant__
 #elif defined(_MSC_VER)
 #define OMEGA_H_INLINE __forceinline
 #define OMEGA_H_INLINE_BIG inline
