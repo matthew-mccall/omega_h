56b95a2c49fefce360f990435f00375c32214caa
Cameron Smith <smithc11@rpi.edu>
2022-08-04 15:25:14 -0400

    offsetscan: 100 iters, timer

--- src/offsetScanPerformance.cpp
+++ src/offsetScanPerformance.cpp
@@ -2,6 +2,7 @@
 #include "Omega_h_library.hpp"
 #include "Omega_h_for.hpp"
 #include "Omega_h_int_scan.hpp"
+#include "Omega_h_profile.hpp"
 
 using namespace Omega_h;
 
@@ -11,6 +12,7 @@ static void test_scan(size_t size) {
   std::stringstream ss;
   ss << "offsetScan_" << size;
   std::string name = ss.str();
+  ScopedTimer scanTime(name.c_str());
   offset_scan(r, name);
 }
 
@@ -18,9 +20,12 @@ int main(int argc, char** argv) {
   auto lib = Library(&argc, &argv);
   OMEGA_H_CHECK(std::string(lib.version()) == OMEGA_H_SEMVER);
   auto size = std::size_t(1) << 21;
+  ScopedTimer totalTime("total time");
   for(int i=0; i<16; i++) {
     size = size >> 1;
     printf("%d size %lu\n", i, size);
-    test_scan(size);
+    for(int j=0; j<100; j++) {
+      test_scan(size);
+    }
   }
 }
