0000ba3463c11899ce0f3b23c1ed5971f23d96d3
Cameron Smith <smithc11@rpi.edu>
2022-12-19 11:49:58 -0500

    compiles

--- src/Omega_h_esmfWrapper.f90
+++ src/Omega_h_esmfWrapper.f90
@@ -1,5 +1,11 @@
 subroutine fooF() bind(C, name='fooFortran')
   use iso_c_binding
+  use ESMF
   implicit none
+  type(ESMF_VM) :: vm
+  integer :: rc, result
+
   write(*,*) 'Fortran fooF'
+  call  ESMF_Initialize(vm=vm, defaultlogfilename="fooF.Log", &
+                    logkindflag=ESMF_LOGKIND_MULTI, rc=rc)
 end
