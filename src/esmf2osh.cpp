5a487c24234d890aa6b152a295ab6ce6a173f2a2
Cameron Smith <smithc11@rpi.edu>
2022-12-20 15:34:20 -0500

    create omegah mesh
    had to move esmffinalize to the end... it was causing mpi problems

--- src/esmf2osh.cpp
+++ src/esmf2osh.cpp
@@ -1,5 +1,6 @@
 #include <Omega_h_library.hpp>
 #include <Omega_h_esmfWrapper.h>
+#include <Omega_h_build.hpp>
 #include <sstream>
 #include <iostream>
 
@@ -15,7 +16,6 @@ int main(int argc, char** argv) {
   esmfGetMeshVtxCoords(coords);
   esmfGetMeshVtxIds(vtxIds);
   esmfGetMeshElemVerts(elemVerts);
-  esmfFinalize();
   std::stringstream ss;
   ss << "vtxIds: ";
   for(int i=0; i<4; i++) {
@@ -37,6 +37,27 @@ int main(int argc, char** argv) {
   }
   ss << "\n";
   std::cout << ss.str();
+
+  //create element-to-vtx device array
+  //the esmf 'elemVerts' array contains indices into the array of vertex ids
+  //the esmf vertex ids start at one instead of zero
+  Omega_h::HostWrite<Omega_h::LO> elemVerts_hw(6);
+  for(int i=0; i<elemVerts_hw.size(); i++)
+    elemVerts_hw[i] = (vtxIds[elemVerts[i]-1]-1);
+  auto elemVerts_dr = Omega_h::read(elemVerts_hw.write());
+
+  Omega_h::HostWrite<Omega_h::Real> vtxCoords_hw(8);
+  for(int i=0; i<vtxCoords_hw.size(); i++)
+    vtxCoords_hw[i] = coords[i];
+  auto vtxCoords_dr = Omega_h::read(vtxCoords_hw.write());
+
+  auto mesh = Omega_h::Mesh(&lib);
+  Omega_h::build_from_elems_and_coords(&mesh, OMEGA_H_SIMPLEX, 2,
+      elemVerts_dr, vtxCoords_dr);
+
   delete [] coords;
+  delete [] elemVerts;
+  delete [] vtxIds;
+  esmfFinalize();
   return 0;
 }
