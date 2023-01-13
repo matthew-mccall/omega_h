3315526c38edeea65bd97dc0954bcdd884a13bb9
Cameron Smith <smithc11@rpi.edu>
2023-01-13 16:37:10 -0500

    const string to loadmesh

--- src/Omega_h_esmfWrapper.h
+++ src/Omega_h_esmfWrapper.h
@@ -3,7 +3,7 @@
 extern "C" void esmfInit();
 extern "C" void esmfFinalize();
 extern "C" void esmfTestMesh();
-extern "C" void esmfLoadMesh(char* meshFileName, int strlen);
+extern "C" void esmfLoadMesh(const char* meshFileName, int strlen);
 extern "C" void esmfGetMeshVtxIds(int* coords); //TODO use GO
 extern "C" void esmfGetMeshVtxCoords(double* coords);
 extern "C" void esmfGetMeshElemVerts(int* elemVerts);
