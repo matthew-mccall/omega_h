2cfefca21440ca97026439dd780895abb66c69b0
Cameron Smith <smithc11@rpi.edu>
2023-01-24 04:11:47 +0000

    simplify

--- src/dbgInvertFanFunnel.cpp
+++ src/dbgInvertFanFunnel.cpp
@@ -6,10 +6,10 @@
 using namespace Omega_h;
 
 static void test_fan_and_funnel() {
-  OMEGA_H_CHECK(invert_funnel(LOs({0, 0, 1, 1, 2, 2}), 3) == LOs({0, 2, 4, 6}));
   OMEGA_H_CHECK(invert_fan(LOs({0, 2, 4, 6})) == LOs({0, 0, 1, 1, 2, 2}));
-  OMEGA_H_CHECK(invert_funnel(LOs({0, 0, 0, 2, 2, 2}), 3) == LOs({0, 3, 3, 6}));
-  OMEGA_H_CHECK(invert_fan(LOs({0, 3, 3, 6})) == LOs({0, 0, 0, 2, 2, 2}));
+  OMEGA_H_CHECK(invert_fan(LOs({0, 2, 4, 6})) == LOs({0, 0, 1, 1, 2, 2}));
+  OMEGA_H_CHECK(invert_fan(LOs({0, 2, 4, 6})) == LOs({0, 0, 1, 1, 2, 2}));
+  OMEGA_H_CHECK(invert_fan(LOs({0, 2, 4, 6})) == LOs({0, 0, 1, 1, 2, 2}));
 }
 
 int main(int argc, char** argv) {
