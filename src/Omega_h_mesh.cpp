d741e2e1c026dfba3b0541ccd505ca47e3f17766
Cameron Smith <smithc11@rpi.edu>
2024-01-22 11:40:07 -0500

    move mixed mesh funcs to Omega_h_mixedMesh.cpp

--- src/Omega_h_mesh.cpp
+++ src/Omega_h_mesh.cpp
@@ -17,8 +17,6 @@
 #include "Omega_h_quality.hpp"
 #include "Omega_h_shape.hpp"
 #include "Omega_h_timer.hpp"
-#include "Omega_h_int_scan.hpp"
-#include "Omega_h_atomics.hpp"
 #include "Omega_h_reduce.hpp"
 #include "Omega_h_print.hpp"
 #include "Omega_h_dbg.hpp"
@@ -29,7 +27,6 @@ Mesh::Mesh() {
   family_ = OMEGA_H_SIMPLEX;
   dim_ = -1;
   for (Int i = 0; i <= 3; ++i) nents_[i] = -1;
-  for (Int i = 0; i <= 7; ++i) nents_type_[i] = -1;
   parting_ = -1;
   nghost_layers_ = -1;
   library_ = nullptr;
@@ -44,10 +41,6 @@ void Mesh::set_library(Library* library_in) {
 }
 
 Library* Mesh::library() const { return library_; }
-
-void Mesh::set_model2d(Model2dPtr const& model2d_in) {
-  model2d_ = model2d_in;
-}
 
 void Mesh::set_comm(CommPtr const& new_comm) {
   auto rank_had_comm = bool(comm_);
@@ -81,7 +74,6 @@ void Mesh::set_comm(CommPtr const& new_comm) {
   comm_ = new_comm;
 }
 
-
 void Mesh::set_family(Omega_h_Family family_in) { family_ = family_in; }
 
 void Mesh::set_matched(I8 is_matched) { matched_ = is_matched; }
@@ -93,26 +85,7 @@ void Mesh::set_dim(Int dim_in) {
   dim_ = dim_in;
 }
 
-Int Mesh::ent_dim(Topo_type ent_type) const {
-  Int ent_dim;
-  if (int(ent_type) == 0) {
-    ent_dim = 0;
-  }
-  else if (int(ent_type) == 1) {
-    ent_dim = 1;
-  }
-  else if ((int(ent_type) > 1) && (int(ent_type) < 4)) {      
-    ent_dim = 2;
-  }
-  else {
-    ent_dim = 3;
-  }
-  return ent_dim;
-}
-
 void Mesh::set_verts(LO nverts_in) { nents_[VERT] = nverts_in; }
-
-void Mesh::set_verts_type(LO nverts_in) { nents_type_[int(Topo_type::vertex)] = nverts_in; }
 
 void Mesh::set_ents(Int ent_dim, Adj down) {
   OMEGA_H_TIME_FUNCTION;
@@ -148,18 +121,6 @@ LOs Mesh::get_model_matches(Int ent_dim) {
   return model_matches_[ent_dim];
 }
 
-void Mesh::set_ents(Topo_type high_type, Topo_type low_type, Adj h2l) {
-  OMEGA_H_TIME_FUNCTION;
-  check_type(high_type);
-  check_type(low_type);
-  if (int(high_type) < 6) {
-    OMEGA_H_CHECK(!has_ents(high_type));
-  }
-  auto deg = element_degree(high_type, low_type);
-  nents_type_[int(high_type)] = divide_no_remainder(h2l.ab2b.size(), deg);
-  add_adj(high_type, low_type, h2l);
-}
-
 void Mesh::set_parents(Int ent_dim, Parents parents) {
   check_dim2(ent_dim);
   parents_[ent_dim] = std::make_shared<Parents>(parents);
@@ -172,11 +133,6 @@ LO Mesh::nents(Int ent_dim) const {
   return nents_[ent_dim];
 }
 
-LO Mesh::nents(Topo_type ent_type) const {
-  check_type2(ent_type);
-  return nents_type_[int(ent_type)];
-}
-
 LO Mesh::nelems() const { return nents(dim()); }
 
 LO Mesh::nregions() const { return nents(REGION); }
@@ -186,34 +142,6 @@ LO Mesh::nfaces() const { return nents(FACE); }
 LO Mesh::nedges() const { return nents(EDGE); }
 
 LO Mesh::nverts() const { return nents(VERT); }
-
-LO Mesh::npyrams() const { return nents(Topo_type::pyramid); }
-
-LO Mesh::nwedges() const { return nents(Topo_type::wedge); }
-
-LO Mesh::nhexs() const { return nents(Topo_type::hexahedron); }
-
-LO Mesh::ntets() const { return nents(Topo_type::tetrahedron); }
-
-LO Mesh::nquads() const { return nents(Topo_type::quadrilateral); }
-
-LO Mesh::ntris() const { return nents(Topo_type::triangle); }
-
-LO Mesh::nedges_mix() const { return nents(Topo_type::edge); }
-
-LO Mesh::nverts_mix() const { return nents(Topo_type::vertex); }
-
-LO Mesh::nregions_mix() const { 
-  return (nents(Topo_type::tetrahedron) +
-          nents(Topo_type::hexahedron) +
-          nents(Topo_type::wedge) +
-          nents(Topo_type::pyramid));
-}
-
-LO Mesh::nfaces_mix() const { 
-  return (nents(Topo_type::triangle) +
-          nents(Topo_type::quadrilateral));
-}
 
 GO Mesh::nglobal_ents(Int ent_dim) {
   if (!could_be_shared(ent_dim)) {
@@ -226,18 +154,6 @@ GO Mesh::nglobal_ents(Int ent_dim) {
 template <typename T>
 void Mesh::add_tag(Int ent_dim, std::string const& name, Int ncomps) {
   this->add_tag(ent_dim, name, ncomps, Read<T>(), true);
-}
-
-template <typename T>
-void Mesh::add_tag(Topo_type ent_type, std::string const& name, Int ncomps) {
-  if (has_tag(ent_type, name)) remove_tag(ent_type, name);
-  check_type2(ent_type);
-  check_tag_name(name);
-  OMEGA_H_CHECK(ncomps >= 0);
-  OMEGA_H_CHECK(ncomps <= Int(INT8_MAX));
-  OMEGA_H_CHECK(tags_type_[int(ent_type)].size() < size_t(INT8_MAX));
-  TagPtr ptr(new Tag<T>(name, ncomps));
-  tags_type_[int(ent_type)].push_back(std::move(ptr));
 }
 
 template <typename T>
@@ -270,28 +186,6 @@ void Mesh::add_tag(Int ent_dim, std::string const& name, Int ncomps,
 }
 
 template <typename T>
-void Mesh::add_tag(Topo_type ent_type, std::string const& name, Int ncomps,
-    Read<T> array, bool internal) {
-  check_type2(ent_type);
-  auto it = tag_iter(ent_type, name);
-  auto had_tag = (it != tags_type_[int(ent_type)].end());
-  auto ptr = std::make_shared<Tag<T>>(name, ncomps);
-  ptr->set_array(array);
-  if (had_tag) {
-    OMEGA_H_CHECK(ncomps == ptr->ncomps());
-    *it = std::move(ptr);
-  } else {
-    check_tag_name(name);
-    OMEGA_H_CHECK(ncomps >= 0);
-    OMEGA_H_CHECK(ncomps <= Int(INT8_MAX));
-    OMEGA_H_CHECK(tags_type_[int(ent_type)].size() < size_t(INT8_MAX));
-    tags_type_[int(ent_type)].push_back(std::move(ptr));
-  }
-  OMEGA_H_CHECK(array.size() == nents_type_[int(ent_type)] * ncomps);
-  if (!internal) react_to_set_tag(ent_type, name);
-}
-
-template <typename T>
 void Mesh::set_tag(
     Int ent_dim, std::string const& name, Read<T> array, bool internal) {
   if (!has_tag(ent_dim, name)) {
@@ -299,17 +193,6 @@ void Mesh::set_tag(
         topological_plural_name(family(), ent_dim), name.c_str());
   }
   this->add_tag(ent_dim, name, divide_no_remainder(array.size(), nents(ent_dim)), array, internal);
-}
-
-template <typename T>
-void Mesh::set_tag(
-    Topo_type ent_type, std::string const& name, Read<T> array, bool internal) {
-  if (!has_tag(ent_type, name)) {
-    Omega_h_fail("set_tag(%s, %s): tag doesn't exist (use add_tag first)\n",
-      dimensional_plural_name(ent_type), name.c_str());
-  }
-  auto it = tag_iter(ent_type,name);
-  this->add_tag(ent_type, name, (*it)->ncomps(), array, internal);
 }
 
 void Mesh::react_to_set_tag(Int ent_dim, std::string const& name) {
@@ -324,28 +207,6 @@ void Mesh::react_to_set_tag(Int ent_dim, std::string const& name) {
   }
 }
 
-void Mesh::react_to_set_tag(Topo_type ent_type, std::string const& name) {
-  bool is_coordinates = (name == "coordinates");
-  if ((int(ent_type) == 0) && (is_coordinates || (name == "metric"))) {
-    remove_tag(Topo_type::edge, "length");
-
-    remove_tag(Topo_type::pyramid, "quality");
-    remove_tag(Topo_type::wedge, "quality");
-    remove_tag(Topo_type::hexahedron, "quality");
-    remove_tag(Topo_type::tetrahedron, "quality");
-    remove_tag(Topo_type::quadrilateral, "quality");
-    remove_tag(Topo_type::triangle, "quality");
-  }
-  if ((int(ent_type) == 0) && is_coordinates) {
-    remove_tag(Topo_type::pyramid, "size");
-    remove_tag(Topo_type::wedge, "size");
-    remove_tag(Topo_type::hexahedron, "size");
-    remove_tag(Topo_type::tetrahedron, "size");
-    remove_tag(Topo_type::quadrilateral, "size");
-    remove_tag(Topo_type::triangle, "size");
-  }
-}
-
 TagBase const* Mesh::get_tagbase(Int ent_dim, std::string const& name) const {
   check_dim2(ent_dim);
   auto it = tag_iter(ent_dim, name);
@@ -356,34 +217,14 @@ TagBase const* Mesh::get_tagbase(Int ent_dim, std::string const& name) const {
   return it->get();
 }
 
-TagBase const* Mesh::get_tagbase(Topo_type ent_type, std::string const& name) const {
-  check_type2(ent_type);
-  auto it = tag_iter(ent_type, name);
-  if (it == tags_type_[int(ent_type)].end()) {
-    Omega_h_fail("get_tagbase(%s, %s): doesn't exist\n",
-        dimensional_plural_name(ent_type), name.c_str());
-  }
-  return it->get();
-}
-
 template <typename T>
 Tag<T> const* Mesh::get_tag(Int ent_dim, std::string const& name) const {
   return as<T>(get_tagbase(ent_dim, name));
 }
 
 template <typename T>
-Tag<T> const* Mesh::get_tag(Topo_type ent_type, std::string const& name) const {
-  return as<T>(get_tagbase(ent_type, name));
-}
-
-template <typename T>
 Read<T> Mesh::get_array(Int ent_dim, std::string const& name) const {
   return get_tag<T>(ent_dim, name)->array();
-}
-
-template <typename T>
-Read<T> Mesh::get_array(Topo_type ent_type, std::string const& name) const {
-  return get_tag<T>(ent_type, name)->array();
 }
 
 void Mesh::remove_tag(Int ent_dim, std::string const& name) {
@@ -392,33 +233,15 @@ void Mesh::remove_tag(Int ent_dim, std::string const& name) {
   tags_[ent_dim].erase(tag_iter(ent_dim, name));
 }
 
-void Mesh::remove_tag(Topo_type ent_type, std::string const& name) {
-  if (!has_tag(ent_type, name)) return;
-  check_type2(ent_type);
-  OMEGA_H_CHECK(has_tag(ent_type, name));
-  tags_type_[int(ent_type)].erase(tag_iter(ent_type, name));
-}
-
 bool Mesh::has_tag(Int ent_dim, std::string const& name) const {
   check_dim(ent_dim);
   if (!has_ents(ent_dim)) return false;
   return tag_iter(ent_dim, name) != tags_[ent_dim].end();
 }
 
-bool Mesh::has_tag(Topo_type ent_type, std::string const& name) const {
-  check_type(ent_type);
-  if (!has_ents(ent_type)) return false;
-  return tag_iter(ent_type, name) != tags_type_[int(ent_type)].end();
-}
-
 Int Mesh::ntags(Int ent_dim) const {
   check_dim2(ent_dim);
   return static_cast<Int>(tags_[ent_dim].size());
-}
-
-Int Mesh::ntags(Topo_type ent_type) const {
-  check_type2(ent_type);
-  return static_cast<Int>(tags_type_[int(ent_type)].size());
 }
 
 TagBase const* Mesh::get_tag(Int ent_dim, Int i) const {
@@ -428,33 +251,15 @@ TagBase const* Mesh::get_tag(Int ent_dim, Int i) const {
   return tags_[ent_dim][static_cast<std::size_t>(i)].get();
 }
 
-TagBase const* Mesh::get_tag(Topo_type ent_type, Int i) const {
-  check_type2(ent_type);
-  OMEGA_H_CHECK(0 <= i);
-  OMEGA_H_CHECK(i <= ntags(ent_type));
-  return tags_type_[int(ent_type)][static_cast<std::size_t>(i)].get();
-}
-
 bool Mesh::has_ents(Int ent_dim) const {
   check_dim(ent_dim);
   return nents_[ent_dim] >= 0;
-}
-
-bool Mesh::has_ents(Topo_type ent_type) const {
-  check_type(ent_type);
-  return nents_type_[int(ent_type)] >= 0;
 }
 
 bool Mesh::has_adj(Int from, Int to) const {
   check_dim(from);
   check_dim(to);
   return bool(adjs_[from][to]);
-}
-
-bool Mesh::has_adj(Topo_type from_type, Topo_type to_type) const {
-  check_type(from_type);
-  check_type(to_type);
-  return bool(adjs_type_[int(from_type)][int(to_type)]);
 }
 
 Adj Mesh::get_adj(Int from, Int to) const {
@@ -464,37 +269,18 @@ Adj Mesh::get_adj(Int from, Int to) const {
   return *(adjs_[from][to]);
 }
 
-Adj Mesh::get_adj(Topo_type from_type, Topo_type to_type) const {
-  check_type2(from_type);
-  check_type2(from_type);
-  OMEGA_H_CHECK(has_adj(from_type, to_type));
-  return *(adjs_type_[int(from_type)][int(to_type)]);
-}
-
 Adj Mesh::ask_down(Int from, Int to) {
   OMEGA_H_CHECK(to < from);
   return ask_adj(from, to);
 }
 
-Adj Mesh::ask_down(Topo_type from_type, Topo_type to_type) {
-  OMEGA_H_CHECK(int(to_type) < int(from_type));
-  return ask_adj(from_type, to_type);
-}
-
 LOs Mesh::ask_verts_of(Int ent_dim) { return ask_adj(ent_dim, VERT).ab2b; }
-
-LOs Mesh::ask_verts_of(Topo_type ent_type) { return ask_adj(ent_type, Topo_type::vertex).ab2b; }
 
 LOs Mesh::ask_elem_verts() { return ask_verts_of(dim()); }
 
 Adj Mesh::ask_up(Int from, Int to) {
   OMEGA_H_CHECK(from < to);
   return ask_adj(from, to);
-}
-
-Adj Mesh::ask_up(Topo_type from_type, Topo_type to_type) {
-  OMEGA_H_CHECK(int(from_type) < int(to_type));
-  return ask_adj(from_type, to_type);
 }
 
 Graph Mesh::ask_star(Int ent_dim) {
@@ -532,16 +318,6 @@ Mesh::TagCIterResult Mesh::rc_tag_iter(Int ent_dim, std::string const& name) con
   return {found, it};
 }
 
-Mesh::TagIter Mesh::tag_iter(Topo_type ent_type, std::string const& name) {
-  return std::find_if(tags_type_[int(ent_type)].begin(), tags_type_[int(ent_type)].end(),
-      [&](TagPtr const& a) { return a->name() == name; });
-}
-
-Mesh::TagCIter Mesh::tag_iter(Topo_type ent_type, std::string const& name) const {
-  return std::find_if(tags_type_[int(ent_type)].begin(), tags_type_[int(ent_type)].end(),
-      [&](TagPtr const& a) { return a->name() == name; });
-}
-
 void Mesh::check_dim(Int ent_dim) const {
   OMEGA_H_CHECK_OP(0, <=, ent_dim);
   OMEGA_H_CHECK_OP(ent_dim, <=, dim());
@@ -550,16 +326,6 @@ void Mesh::check_dim(Int ent_dim) const {
 void Mesh::check_dim2(Int ent_dim) const {
   check_dim(ent_dim);
   OMEGA_H_CHECK(has_ents(ent_dim));
-}
-
-void Mesh::check_type(Topo_type ent_type) const {
-  OMEGA_H_CHECK(Topo_type::vertex <= ent_type);
-  OMEGA_H_CHECK(ent_type <= Topo_type::pyramid);
-}
-
-void Mesh::check_type2(Topo_type ent_type) const {
-  check_type(ent_type);
-  OMEGA_H_CHECK(has_ents(ent_type));
 }
 
 void Mesh::add_adj(Int from, Int to, Adj adj) {
@@ -585,34 +351,6 @@ void Mesh::add_adj(Int from, Int to, Adj adj) {
     OMEGA_H_CHECK(adj.a2ab.size() == nents(from) + 1);
   }
   adjs_[from][to] = std::make_shared<Adj>(adj);
-}
-
-void Mesh::add_adj(Topo_type from_type, Topo_type to_type, Adj adj) {
-  check_type2(from_type);
-  check_type(to_type);
-  OMEGA_H_CHECK(adj.ab2b.exists());
-  const int from = int(from_type);
-  const int to = int(to_type); 
-
-  if (to < from) {
-    OMEGA_H_CHECK(!adj.a2ab.exists());                         
-    if (to_type == Topo_type::vertex) {
-      OMEGA_H_CHECK(!adj.codes.exists());
-    } else {
-      OMEGA_H_CHECK(adj.codes.exists());
-    }
-    OMEGA_H_CHECK(                                            
-        adj.ab2b.size() == nents(from_type) * element_degree(from_type, to_type));
-  } else {
-    if (from < to) {
-      OMEGA_H_CHECK(adj.a2ab.exists());                        
-      OMEGA_H_CHECK(adj.codes.exists());
-      OMEGA_H_CHECK(
-          adj.ab2b.size() == nents(to_type) * element_degree(to_type, from_type)); 
-    }
-    OMEGA_H_CHECK(adj.a2ab.size() == nents(from_type) + 1);         
-  }
-  adjs_type_[from][to] = std::make_shared<Adj>(adj);
 }
 
 Adj Mesh::derive_adj(Int from, Int to) {
@@ -655,44 +393,6 @@ Adj Mesh::derive_adj(Int from, Int to) {
   OMEGA_H_NORETURN(Adj());
 }
 
-Adj Mesh::derive_adj(Topo_type from_type, Topo_type to_type) {
-  OMEGA_H_TIME_FUNCTION;
-  check_type(from_type);
-  check_type2(to_type);
-  const int from = int(from_type);
-  const int to = int(to_type);
-  if (from < to) {
-    Adj down = ask_adj(to_type, from_type);
-    Int nlows_per_high = element_degree(to_type, from_type);
-    LO nlows = nents(from_type);
-    Adj up = invert_adj(down, nlows_per_high, nlows, to_type, from_type);
-    return up;
-  }
-  else if (to < from) {
-    OMEGA_H_CHECK(to + 1 < from);
-    Topo_type mid_type;
-    if (to_type == Topo_type::vertex) {
-      mid_type = Topo_type::edge;
-    }
-    else if ((from_type == Topo_type::tetrahedron) ||
-             (from_type == Topo_type::pyramid)) {
-      mid_type = Topo_type::triangle;
-    }
-    else {
-      mid_type = Topo_type::quadrilateral;
-    }
-    Adj h2m = ask_adj(from_type, mid_type);
-    Adj m2l = ask_adj(mid_type, to_type);
-    Adj h2l = transit(h2m, m2l, from_type, to_type, mid_type); 
-    return h2l;
-  }
-  /* todo: add second order adjacency derivation */
-  Omega_h_fail("can't derive adjacency from %s to %s\n",
-      dimensional_plural_name(from_type),
-      dimensional_plural_name(to_type));
-  OMEGA_H_NORETURN(Adj());
-}
-
 Adj Mesh::ask_adj(Int from, Int to) {
   OMEGA_H_TIME_FUNCTION;
   check_dim2(from);
@@ -702,18 +402,6 @@ Adj Mesh::ask_adj(Int from, Int to) {
   }
   Adj derived = derive_adj(from, to);
   adjs_[from][to] = std::make_shared<Adj>(derived);
-  return derived;
-}
-
-Adj Mesh::ask_adj(Topo_type from_type, Topo_type to_type) {
-  OMEGA_H_TIME_FUNCTION;
-  check_type2(from_type);
-  check_type2(to_type);
-  if (has_adj(from_type, to_type)) {
-    return get_adj(from_type, to_type);
-  }
-  Adj derived = derive_adj(from_type, to_type);
-  adjs_type_[int(from_type)][int(to_type)] = std::make_shared<Adj>(derived);
   return derived;
 }
 
@@ -727,12 +415,6 @@ void Mesh::set_coords(Reals const& array) {
   OMEGA_H_CHECK(array.size() == nverts() * dim());
   set_tag<Real>(VERT, "coordinates", array);
 }
-
-void Mesh::add_coords_mix(Reals array) {
-  add_tag<Real>(Topo_type::vertex, "coordinates", dim(), array);
-}
-
-Reals Mesh::coords_mix() const { return get_array<Real>(Topo_type::vertex, "coordinates"); }
 
 Read<GO> Mesh::globals(Int ent_dim) const {
   return get_array<GO>(ent_dim, "global");
@@ -1367,13 +1049,6 @@ void get_all_dim_tags(Mesh* mesh, Int dim, TagSet* tags) {
   }
 }
 
-void get_all_type_tags(Mesh* mesh, Int dim, Topo_type ent_type, TagSet* tags) {
-  for (Int j = 0; j < mesh->ntags(ent_type); ++j) {
-    auto tagbase = mesh->get_tag(ent_type, j);
-    (*tags)[size_t(dim)].insert(tagbase->name());
-  }
-}
-
 TagSet get_all_mesh_tags(Mesh* mesh) {
   TagSet out;
   for (Int i = 0; i <= mesh->dim(); ++i) {
@@ -1435,22 +1110,13 @@ __host__
 #define OMEGA_H_INST(T)                                                        \
   template Tag<T> const* Mesh::get_tag<T>(Int dim, std::string const& name)    \
       const;                                                                   \
-  template Tag<T> const* Mesh::get_tag<T>(Topo_type ent_type, std::string const& name)    \
-      const;                                                                   \
   template Read<T> Mesh::get_array<T>(Int dim, std::string const& name) const; \
-  template Read<T> Mesh::get_array<T>(Topo_type ent_type, std::string const& name) const; \
   template void Mesh::add_tag<T>(                                              \
       Int dim, std::string const& name, Int ncomps);                           \
-  template void Mesh::add_tag<T>(                                              \
-      Topo_type ent_type, std::string const& name, Int ncomps);                           \
   template void Mesh::add_tag<T>(Int dim, std::string const& name, Int ncomps, \
-      Read<T> array, bool internal);                                           \
-  template void Mesh::add_tag<T>(Topo_type ent_type, std::string const& name, Int ncomps, \
       Read<T> array, bool internal);                                           \
   template void Mesh::set_tag(                                                 \
       Int dim, std::string const& name, Read<T> array, bool internal);         \
-  template void Mesh::set_tag(                                                 \
-      Topo_type ent_type, std::string const& name, Read<T> array, bool internal);         \
   template Read<T> Mesh::sync_array(Int ent_dim, Read<T> a, Int width);        \
   template Future<T> Mesh::isync_array(Int ent_dim, Read<T> a, Int width);     \
   template Read<T> Mesh::owned_array(Int ent_dim, Read<T> a, Int width);       \
