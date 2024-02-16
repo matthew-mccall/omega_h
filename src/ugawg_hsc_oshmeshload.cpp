671e4d5064e185f3816c696a071f842ca294eaa4
Cameron Smith <smithc11@rpi.edu>
2024-02-16 12:16:13 -0500

    ugawg_hsc driver that loads osh mesh
    need to add code to assert the tags exist and come up with a better name

---
+++ src/ugawg_hsc_oshmeshload.cpp
@@ -0,0 +1,32 @@
+#include <Omega_h_adapt.hpp>
+#include <Omega_h_cmdline.hpp>
+#include <Omega_h_file.hpp>
+#include <Omega_h_library.hpp>
+#include <Omega_h_mesh.hpp>
+#include <Omega_h_metric.hpp>
+#include <Omega_h_vtk.hpp>
+
+int main(int argc, char** argv) {
+  auto lib = Omega_h::Library(&argc, &argv);
+  Omega_h::CmdLine cmdline;
+  cmdline.add_arg<std::string>("mesh_in.meshb");
+  if (!cmdline.parse_final(lib.world(), &argc, argv)) return -1;
+  auto mesh_path = cmdline.get<std::string>("mesh_in.meshb");
+  Omega_h::Mesh mesh(&lib);
+  Omega_h::binary::read(mesh_path.c_str(), lib.world(), &mesh);
+  fprintf(stderr, "nverts %d nelms %d\n", mesh.nverts(), mesh.nelems());
+  
+  Omega_h::add_implied_metric_tag(&mesh);
+  mesh.ask_qualities();
+  auto opts = Omega_h::AdaptOpts(&mesh);
+  opts.xfer_opts.type_map["mach"] = OMEGA_H_LINEAR_INTERP;
+  opts.min_quality_allowed = 0.07;
+
+  opts.verbosity = Omega_h::EXTRA_STATS;
+  while (Omega_h::approach_metric(&mesh, opts)) {
+    Omega_h::adapt(&mesh, opts);
+  }
+
+  Omega_h::vtk::write_parallel("deltaWing_adapted_omegah.vtk", &mesh);
+  return 0;
+}
